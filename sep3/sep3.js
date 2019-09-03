6d1775746212184ed850b67c69cc0fda7229cda0
JC <james.calingo@gmail.com>
2019-09-03 09:40:23 -0400

    sep 3 update

--- sep3/sep3.js
+++ sep3/sep3.js
@@ -3,11 +3,35 @@ $(document).on("click", function (event){
   event.preventDefault();
   counter++;
   console.log(counter)
+
   if(counter===1){
-  $("#magiczone").append("Now, I want you do do something.<br>");
-  $("#continue").append("(click the page to continue.)")
+  $("#magiczone").append(`<h3>Now, I want you do a few things.</h3><br>`);
+  $("#continue").html("(click the page to continue...but only when you're ready.)");
   }
+
   if(counter === 2){
-    $("#magiczone").append("You should have a new number right now. Take that number")
+    $("#magiczone").append(`<h4>Take your number and multiply it by 9. If the resulting number is two digits, then add those digits together.</h4>`);
   }
+
+  if(counter === 3){
+    $("#magiczone").append(`<h4>You should have a single digit number now. Take that and add your choice of either 15 or 36 to it.</h4>`);
+  }
+
+  if(counter === 4){
+    $("#magiczone").append(`<h4>If your result is odd, divide it by 15. If it's even, divide by 8.</h4>`);
+  }
+
+  if(counter === 5){
+    $("#magiczone").append(`<h4>This is where things get a little tricky. I want you to think of a color that has as many letters as the number you have right now (for example, if you have 5 as your number, you'd pick a color with three letters such as green).</h4>`);
+  };
+
+  if(counter === 6){
+    $("#magiczone").append(`<h4>The next time you click, I will turn this entire page to the color you are thinking of.</h4><h1>Ready?</h1>`);
+  };
+
+  if(counter===7){
+    $("body").css({"background":"red", "color":"white"});
+    $("#magiczone").html(`<h1 class="display-4">BAM!</h1><h3>Am I right?`);
+    $("#continue").html(`<p>Hope you enjoyed this!</p>`);
+  };
 })
---
+++ sep5/sep5.js
@@ -0,0 +1,8 @@
+let away=0;
+let home=0;
+let down=1;
+let distance=10
+$("#down").html(`Down: ${down}`)
+$("#distance").html(`Yards to go: ${distance}`)
+$(".awayscore").html(away)
+$(".homescore").html(home)
