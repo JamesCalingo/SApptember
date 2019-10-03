7c8e04a4494069b61dba850a32555d8b0910d3aa
JC <james.calingo@gmail.com>
2019-10-02 22:34:42 -0400

    updated scoreboard app

--- sep5/sep5.js
+++ sep5/sep5.js
@@ -3,25 +3,32 @@ let home = 0;
 let down = 1;
 let distance = 10;
 let buttonPress;
-$("#down").html(`Down: ${down}`)
-$("#distance").html(`Yards to go: ${distance}`)
-$(".awayscore").html(away)
-$(".homescore").html(home)
+$("#down").html(`Down: ${down}`);
+$("#distance").html(`Yards to go: ${distance}`);
+$(".awayscore").html(away);
+$(".homescore").html(home);
+
+firstdown = () =>{
+  down = 1;
+  distance = 10;
+  $("#down").html(`Down: 1`);
+  $("#distance").html(`Yards to go: 10`);
+};
 
 $(".scorebtna").on("click", function (event) {
   event.preventDefault();
-  buttonPress = parseInt($(this).attr("data-attribute"))
+  buttonPress = parseInt($(this).attr("data-attribute"));
   away += buttonPress;
-  console.log(away)
-  $(".awayscore").html(away)
+  console.log(away);
+  $(".awayscore").html(away);
 })
 
 $(".scorebtnh").on("click", function (event) {
   event.preventDefault();
-  buttonPress = parseInt($(this).attr("data-attribute"))
+  buttonPress = parseInt($(this).attr("data-attribute"));
   home += buttonPress;
-  console.log(home)
-  $(".homescore").html(home)
+  console.log(home);
+  $(".homescore").html(home);
 })
 
 $("#changedown").on("click", function (event) {
@@ -29,28 +36,29 @@ $("#changedown").on("click", function (event) {
   down++;
   $("#down").html(`Down: ${down}`);
   if (down > 4) {
-    down = 1;
-    $("#down").html(`Down: 1`);
+    alert("There is no code of gridiron football that goes past four downs. Resetting to first down and ten yards to go.");
+  firstdown();
   }
 })
 
 $("#firstdown").on("click", function (event) {
   event.preventDefault();
-  down = 1;
-  distance = 10;
-  $("#down").html(`Down: 1`);
-  $("#distance").html(`Yards to go: 10`)
-
+ firstdown();
 })
 
 $("#addyds").on("click", function(){
   distance++;
-  $("#distance").html(`Yards to go: ${distance}`)
+  $("#distance").html(`Yards to go: ${distance}`);
 })
 
 $("#subyds").on("click", function(){
+  if(distance <= 0){
+    alert("You can't have negative yards!");
+    return false;
+  }else{
   distance--;
-  $("#distance").html(`Yards to go: ${distance}`)
+  $("#distance").html(`Yards to go: ${distance}`);
+  }
 })
 
 $("#reset").on("click", function () {
